74efdbf7194065b0a6fb67d3d3755564cf562c4c
Tomas <tomas@Tomass-iMac-Pro.local>
2020-04-08 13:42:46 +0100

    Spring MVC Skeleton

---
+++ G00361128/src/main/java/com/example/demo/G00361128Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G00361128Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G00361128Application.class, args);
+	}
+
+}
